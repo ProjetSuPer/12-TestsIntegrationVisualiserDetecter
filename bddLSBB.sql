599c4e05394e5f41f23f50ae60f8005b96d9c883
Brice Jamin <brice.jamin@hotmail.fr>
2013-05-24 13:58:09 +0200

    [New] Integration script bdd.

--- bddLSBB.sql
+++ bddLSBB.sql
@@ -41,6 +41,7 @@ CREATE TABLE vue
 (
    num_vue	INT UNSIGNED	NOT NULL,
    legende	VARCHAR(100)	NOT NULL,
+   image	VARCHAR(100)	NOT NULL,
 
    PRIMARY KEY (num_vue)
    
@@ -51,14 +52,95 @@ DROP TABLE IF EXISTS representationLieuSurVue;
 
 CREATE TABLE representationLieuSurVue
 (
-   num_vue	INT UNSIGNED	NOT NULL,
-   num_lieu	INT UNSIGNED	NOT NULL,
+   num_vue	INT UNSIGNED 	NOT NULL,
+   num_lieu	INT UNSIGNED 	NOT NULL,
+   num_zone	INT UNSIGNED 	NOT NULL,
    x		INT		NOT NULL,
    y		INT		NOT NULL,
-   
-   PRIMARY KEY (num_vue, num_lieu)
-   
+   xA		INT		NOT NULL,
+   yA		INT		NOT NULL,
+   xB		INT		NOT NULL,
+   yB		INT		NOT NULL,
+
+   PRIMARY KEY (num_vue, num_lieu, num_zone)
+
 )ENGINE=INNODB;
+
+DROP TABLE IF EXISTS personne;
+
+CREATE TABLE personne
+(
+   nom		VARCHAR(50)	NOT NULL,
+   prenom	VARCHAR(50)	NOT NULL,
+   societe	VARCHAR(50)	NOT NULL,
+   dateDebut	DATE		NOT NULL,
+   dateFin	DATE		NOT NULL,
+   num_pers	INT UNSIGNED 	NOT NULL,
+   photo	VARCHAR(100)	NOT NULL,
+
+   PRIMARY KEY (num_pers)
+
+)ENGINE=INNODB;
+
+DROP TABLE IF EXISTS badge;
+
+CREATE TABLE badge
+(
+   num_badge	INT UNSIGNED	NOT NULL,
+   num_pers	INT UNSIGNED	NOT NULL,
+   dateMiseEnService	DATE	NOT NULL,
+   dateChangePile	DATE	NOT NULL,
+   estActif	TINYINT UNSIGNED	NOT NULL,
+   constraint estActif_is_bool	check(estActif BETWEEN 0 and 1),
+
+   PRIMARY KEY (num_badge, num_pers)
+
+)ENGINE=INNODB;
+
+DROP TABLE IF EXISTS zone;
+
+CREATE TABLE zone
+(
+   num_zone	INT UNSIGNED	NOT NULL,
+   num_lieu	INT UNSIGNED	NOT NULL,
+   sensMonter	INT UNSIGNED	NOT NULL,
+   legende	VARCHAR(100)	NOT NULL,
+
+   PRIMARY KEY (num_zone, num_lieu)
+
+)ENGINE=INNODB;
+
+CREATE TABLE log
+(
+   num_badge	INT UNSIGNED	NOT NULL,
+   dateLog	VARCHAR(100)	NOT NULL,
+   legende	VARCHAR(100)	NOT NULL,
+
+   PRIMARY KEY (dateLog, num_badge)
+
+)ENGINE=INNODB;
+
+CREATE TABLE super
+(
+   config	INT UNSIGNED	NOT NULL,
+   tempoM	INT UNSIGNED	NOT NULL,
+   tempoR	INT UNSIGNED	NOT NULL,
+
+   PRIMARY KEY (config)
+
+)ENGINE=INNODB;
+
+
+
+-- Interdiction de supprimer un lieu auquel une zone est liée
+-- Un lieu modifié est aussi modifié dans zone
+alter table zone add constraint fk_zone_num_lieu foreign key (num_lieu)
+      references lieu (num_lieu) on delete restrict on update cascade;
+
+-- Interdiction de supprimer une personne auquel un badge est lié
+-- Une personne modifiée est aussi modifiée dans badge
+alter table badge add constraint fk_badge_num_pers foreign key (num_pers)
+      references personne (num_pers) on delete restrict on update cascade;
 
 -- Interdiction de supprimer un lieu auquel un lecteur est lié
 -- Un lieu modifié est aussi modifié dans lecteur
@@ -75,27 +157,40 @@ alter table representationLieuSurVue add constraint fk_representationLieuSurVue_
 
 
 INSERT INTO lieu (num_lieu, legende) VALUE
-  (1, "Entrée tunnel zone 1"),
+  (1, "Entree tunnel zone 1"),
   (2, "Angle tunnel zone 2"),
-  (3, "Dans tunnel zone 3, en face des dépendances");
+  (3, "Dans tunnel zone 3, en face des dependances");
 
 
 INSERT INTO lecteur (num_lecteur, num_lieu, ip, estConnecte) VALUE
-  (1, 1, '127.0.0.1', 0),
-  (2, 2, '127.0.0.2', 0),
-  (3, 3, '127.0.0.3', 0);
+  (1, 1, '192.168.0.1', 0),
+  (2, 2, '192.168.0.2', 0),
+  (3, 3, '192.168.0.3', 0);
 
 
-INSERT INTO vue (num_vue, legende) VALUE
-  (1, "Vue globale du site représentant tous les lieux"),
-  (2, "Vue zoomant sur les secteurs 1, 2 et 3, de l'entrée aux dépendances.");
+INSERT INTO vue (num_vue, legende, image) VALUE
+  (1, "Vue globale", "../ressources/vue_generale.jpg"),
+  (2, "Vue zoomant de l'entree aux dependances", "../ressources/vue_lieu_1-2-3.jpg");
 
 
-INSERT INTO representationLieuSurVue (num_vue, num_lieu, x, y) VALUE
-  (1, 1, 0, 0),
-  (1, 2, 0, 0),
-  (1, 3, 0, 0),
-  (2, 1, 0, 0),
-  (2, 2, 0, 0),
-  (2, 3, 0, 0);
+INSERT INTO representationLieuSurVue (num_vue, num_lieu, num_zone, x, y, xA, yA, xB, yB) VALUE
+  (1, 1, 1, 580, 472, 570, 530, 0, 0),
+  (1, 2, 2, 540, 442, 540, 510, 0, 0),
+  (1, 2, 23, 540, 442, 530, 490, 0, 0),
+  (1, 3, 3, 520, 432, 480, 450, 0, 0),
+  (2, 1, 1, 580, 488, 550, 530, 400, 390),
+  (2, 2, 2, 220, 128, 400, 390, 200, 190),
+  (2, 2, 23, 220, 128, 200, 190, 40, 190),
+  (2, 3, 3, 60, 28, 40, 190, 41, 10);
 
+INSERT INTO personne (nom, prenom, societe, dateDebut, dateFin, num_pers) VALUE
+  ("scherer", "nicolas", "LAB", 0, 0, 1);
+
+INSERT INTO badge (num_badge, num_pers, dateMiseEnService, dateChangePile, estActif) VALUE
+  (01, 1, 0, 0, 0);
+
+INSERT INTO zone (num_zone, num_lieu,sensMonter, legende) VALUE
+  (1, 1, 2, "reception uniquement lecteur 1"),
+  (2, 2, 1, "reception uniquement lecteur 2"),
+  (23, 2, 3, "reception lecteur 2 et lecteur 3"),
+  (3, 3, 1, "reception uniquement lecteur 3");
