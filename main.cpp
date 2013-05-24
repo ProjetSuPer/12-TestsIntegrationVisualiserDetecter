533af8cfa8f0246877a3b577280773ca62b50782
Brice Jamin <brice.jamin@hotmail.fr>
2013-05-24 12:32:13 +0200

    [New] Integration main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <QApplication>
 #include <QLabel>
+#include <QTextCodec>
 #include "ihm.h"
 
 int main(int argc, char *argv[])
 {
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+
     QApplication a(argc, argv);
+    qDebug();
+
+    //Server server("192.168.60.100", "2222");
+
     Ihm w;
     w.show();
 
-    
     return a.exec();
 }
 
