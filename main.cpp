b0536c07001dbf10282996306eebe0f77692422f
BriceJamin <brice.jamin@hotmail.fr>
2013-05-30 16:43:15 +0200

    [Bug] Plantage du programme au démarrage.
    Les ajouts de qDebug() dans le main.cpp révèlent que le plantage
    surgit dans le code de l'Ihm.

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@
 
 int main(int argc, char *argv[])
 {
+    qDebug();
+
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
 
+    qDebug() << QThread::currentThreadId() << Q_FUNC_INFO << "QApplication a(argc, argv);";
     QApplication a(argc, argv);
-    qDebug();
 
+    qDebug() << QThread::currentThreadId() << Q_FUNC_INFO << "Server server(\"192.168.60.100\", \"2222\");";
     Server server("192.168.60.100", "2222");
 
+    qDebug() << QThread::currentThreadId() << Q_FUNC_INFO << "Ihm w(&server);";
     Ihm w(&server);
+
+    qDebug() << QThread::currentThreadId() << Q_FUNC_INFO << "w.show();";
     w.show();
 
     return a.exec();
